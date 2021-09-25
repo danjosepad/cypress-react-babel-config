e08dca2519cd56dd0bf5805cdc0fc7b2d7ab5f7a
daniel.padilha <danielpadilhapessoal@outlook.com>
2021-09-25 14:47:11 -0300

    feat: add changes on calculator display

--- src/shared/__tests__/calculator-display.js
+++ src/shared/__tests__/calculator-display.js
@@ -2,9 +2,20 @@ import React from 'react'
 import {render} from '@testing-library/react'
 import CalculatorDisplay from '../calculator-display'
 
+// jest-emotion (Snapshot serializer)
+
 test('renders', () => {
   const {container} = render(<CalculatorDisplay value="0" />)
-  expect(container.innerHTML).toMatchInlineSnapshot(
-    `"<div class=\\"css-12149uf\\"><div class=\\"autoScalingText\\" style=\\"transform: scale(1,1);\\">0</div></div>"`,
-  )
+  expect(container.firstChild).toMatchInlineSnapshot(`
+<div
+  class="css-12149uf"
+>
+  <div
+    class="autoScalingText"
+    style="transform: scale(1,1);"
+  >
+    0
+  </div>
+</div>
+`)
 })
