2a97bb66ea58a16379666ea9943ceeb8131ac061
daniel.padilha <danielpadilhapessoal@outlook.com>
2021-10-01 22:13:09 -0300

    feat: add changes on calculator test

--- src/__tests__/calculator.js
+++ src/__tests__/calculator.js
@@ -1,14 +1,19 @@
 import React from 'react'
+import * as jestDOM from '@testing-library/jest-dom' // Testing library is already using this library, but ill use it as an example anyway
+// import * as jestDOM from '@testing-library/jest-dom/extend-expect' can be used instead of the import above, so we don't need the extend.expect boilerplate 
+
 import { fireEvent, render, screen } from '@testing-library/react'
 import Calculator from '../calculator'
+
+expect.extend(jestDOM) // Now we can use more assertions
 
 test('the clear button switches from AC to C when there is an entry', () => {
   render(<Calculator />)
   const clearButton = screen.getByText('AC')
 
   fireEvent.click(screen.getByText(/3/))
-  expect(clearButton.textContent).toBe('C')
+  expect(clearButton).toHaveTextContent('C')
 
   fireEvent.click(clearButton)
-  expect(clearButton.textContent).toBe('AC')
+  expect(clearButton).toHaveTextContent('AC') // toHaveTextContent from jestDOM
 })
